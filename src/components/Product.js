24bafdb33e3fffd42153345b85c55aba7bacf935
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-02-05 12:37:36 -0300

    Create ProductListing and Product components

---
+++ src/components/Product.js
@@ -0,0 +1,7 @@
+export const Product = () => {
+  return (
+    <article>
+      <h2>Product</h2>
+    </article>
+  );
+};
---
+++ src/components/ProductListing.js
@@ -0,0 +1,7 @@
+export const ProductListing = () => {
+  return (
+    <section className="">
+      <h1>Product Listing</h1>
+    </section>
+  );
+};
