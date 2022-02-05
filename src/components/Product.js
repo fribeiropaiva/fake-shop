304a35efa9339fdffb64f66a6ea7d9d2fadddab6
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-02-05 13:29:21 -0300

    Create Product structure

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
+
 export const Product = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const { id, title } = products[0];
   return (
-    <article>
-      <h2>Product</h2>
+    <article className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header">{title}</div>
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
--- src/components/ProductListing/index.js
+++ src/components/ProductListing/index.js
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
+import { Product } from "../Product";
 
 export const ProductListing = () => {
   const products = useSelector((state) => state);
   return (
     <section className="ui grid container">
-      <h1>Product Listing</h1>
+      <Product />
     </section>
   );
 };
