1bbaf1395670e3a28d06ce7994a608cb5d6bc53a
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-02-06 09:10:34 -0300

    Fix product reducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,19 +1,13 @@
 import { ActionTypes } from "../constants/actionTypes";
 
 const initialState = {
-  products: [
-    {
-      id: 1,
-      title: "Filipe",
-      category: "Front End Developer"
-    }
-  ]
+  products: []
 };
 
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case type === ActionTypes.SET_PRODUCT:
-      return { ...state, products: [...state.products, payload] };
+    case type === ActionTypes.SET_PRODUCTS:
+      return { ...state, products: payload };
     default:
       return state;
   }
