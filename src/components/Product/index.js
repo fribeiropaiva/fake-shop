ff05e9c121f373738f2e91f527a4db094bd05f36
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-02-06 11:25:22 -0300

    Fix Product's styles

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 export const Product = ({ title, price, category, image, id }) => {
   return (
-    <Link to={`/product/${id}`}>
-      <article className="ui link cards">
-        <div className="card">
-          <div className="image">
+    <Link className={styles.link} to={`/product/${id}`}>
+      <article className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles.image}>
             <img src={image} alt={title} />
           </div>
-          <div className="content">
-            <div className="header">{title}</div>
-            <div className="meta price">$ {price}</div>
-            <div className="meta">{category}</div>
+          <div className={styles.content}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.price}>$ {price}</div>
+            <div className={styles.category}>{category}</div>
           </div>
         </div>
       </article>
