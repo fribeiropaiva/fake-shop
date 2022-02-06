25d804c65f30abf02cf1767912767eaf23f737ca
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-02-06 09:25:06 -0300

    Add ProductPage styles

--- src/components/ProductPage/index.js
+++ src/components/ProductPage/index.js
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../../redux/actions/productsActions";
-import { Product } from "../Product";
 import axios from "axios";
 import { ProductListing } from "../ProductListing";
+import styles from "./styles.module.css";
 
 export const ProductPage = () => {
   const dispatch = useDispatch();
@@ -20,6 +20,6 @@ export const ProductPage = () => {
     fetchProducts();
   }, []);
   return (
-    <section className="ui grid container">{/* <ProductListing /> */}</section>
+    <section className={styles.container}>{/* <ProductListing /> */}</section>
   );
 };
